0542a8e4c60835a5c1c38e99c700c9ba1799272f
卫川 <weiguo.zhuwg@antfin.com>
2019-01-24 00:47:24 +0800

    add mock server for testing grpc client

---
+++ tests/mock_servicer.py
@@ -0,0 +1,24 @@
+import grpc
+from concurrent import futures
+
+from google.protobuf.empty_pb2 import Empty
+import sqlflow.proto.sqlflow_pb2_grpc as pb_grpc
+
+class MockServicer(pb_grpc.SQLFlowServicer):
+    """
+    server implementation
+    """
+    def Run(self, request, context):
+        return Empty()
+
+
+def _server(port, event):
+    svr = grpc.server(futures.ThreadPoolExecutor(max_workers=16))
+    pb_grpc.add_SQLFlowServicer_to_server(MockServicer(), svr)
+    svr.add_insecure_port("[::]:%d" % port)
+    svr.start()
+    try:
+        event.wait()
+    except KeyboardInterrupt:
+        pass
+    svr.stop(0)
